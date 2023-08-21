1fcfdcbb1b87d2e98b428cc76c47198907116725
qqJIeee <138237629+qqJIeee@users.noreply.github.com>
2023-08-21 19:37:39 +0300

    Update mlimits.py

--- mlimits.py
+++ mlimits.py
@@ -297,11 +297,11 @@ class mlimits(loader.Module):
             self.limitsxx = True
             timee = time[-1]
             if timee in ['1']:
-                await utils.answer(message, f"✅ Автоматическая установка лимита игроку <code>{chel}</code> раз в <code>{time}</code> секунду начата {kolvo} раз")
+                await utils.answer(message, f"✅ Автоматическая установка лимита игроку <code>{chel}</code> раз в <code>{time}</code> секунду {kolvo} раз начата")
             if timee in ['2', '3', '4']:
-                await utils.answer(message, f"✅ Автоматическая установка лимита игроку <code>{chel}</code> раз в <code>{time}</code> секунды начата {kolvo} раз")
+                await utils.answer(message, f"✅ Автоматическая установка лимита игроку <code>{chel}</code> раз в <code>{time}</code> секунды {kolvo} раз начата")
             if timee in ['5', '6', '7', '8', '9', '0']:
-                await utils.answer(message, f"✅ Автоматическая установка лимита игроку <code>{chel}</code> раз в <code>{time}</code> секунд начата {kolvo} раз")
+                await utils.answer(message, f"✅ Автоматическая установка лимита игроку <code>{chel}</code> раз в <code>{time}</code> секунд {kolvo} раз начата ")
             time = int(time)
             if self.limitsxx:
                 while self.limitsxx:
