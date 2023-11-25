22c23614a194535460517e66de164c4a52ea6333
qqJIeee <138237629+qqJIeee@users.noreply.github.com>
2023-11-25 13:49:01 +0300

    Update mlogs.py

--- mlogs.py
+++ mlogs.py
@@ -161,7 +161,7 @@ class mlogs(loader.Module):
     @loader.watcher()
     async def watcher(self, message):
         chid = int(self.get("chid"))
-        if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "Найден" in message.raw_text or message.chat_id == -1001951117960 and "Найден" in message.raw_text:
+        if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "Найден" in message.raw_text:
             if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "✉" in message.raw_text and "Конверт" in message.raw_text:
                 if self.get('kt'):
                     colpt = r"\d+"
@@ -190,7 +190,7 @@ class mlogs(loader.Module):
                     colvo = search[0]
                     await self.inline.bot.send_message(chid, self.get('rks').format(colvo=colvo))
 
-            if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "🕋" in message.raw_text and "Мифический Кейс" in message.raw_text or message.chat_id == -1001951117960 and "Мифический Кейс" in message.raw_text:
+            if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "🕋" in message.raw_text and "Мифический Кейс" in message.raw_text:
                 if self.get('mif'):         
                     colpt = r"\d+"
                     search = re.search(colpt, message.raw_text)
