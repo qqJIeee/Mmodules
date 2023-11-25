dd870ba7140e87b41486d5b762c7a92a653caf3c
qqJIeee <138237629+qqJIeee@users.noreply.github.com>
2023-11-25 18:53:37 +0300

    Update mlogs.py

--- mlogs.py
+++ mlogs.py
@@ -197,7 +197,7 @@ class mlogs(loader.Module):
                     colvo = search[0]
                     await self.inline.bot.send_message(chid, self.get('mifs').format(colvo=colvo))
             
-            if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "Портфель" in message.raw_text: or message.chat_id == -1001951117960 and "Портфель" in message.raw_text:
+            if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "Портфель" in message.raw_text or message.chat_id == -1001951117960 and "Портфель" in message.raw_text:
                 if self.get('ps'):
                     colpt = r"\d+"
                     search = re.search(colpt, message.raw_text)
