353d9622ce954fac68f50f09cd87e4ec97f7bfcf
qqJIeee <138237629+qqJIeee@users.noreply.github.com>
2023-12-14 19:44:12 +0300

    Update mlogs.py

--- mlogs.py
+++ mlogs.py
@@ -228,7 +228,7 @@ class mlogs(loader.Module):
                     colvo = search[0]
                     await self.inline.bot.send_message(chid, self.get('dks').format(colvo=colvo))  
 
-           if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "💫" in message.raw_text:
+            if hasattr(message, 'from_id') and message.from_id == 5522271758 and message.chat_id == 5522271758 and "💫" in message.raw_text:
                 if self.get('zv'):
                     colvo = 1
                     await self.inline.bot.send_message(chid, self.get('zvs').format(colvo=colvo))
