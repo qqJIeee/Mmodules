8e22245c8db78d32337c0ea68272d0b3122db3b5
qqJIeee <mhabar08@mail.ru>
2024-03-14 13:41:14 +0300

    Update mlogs_main.py

--- mlogs_main.py
+++ mlogs_main.py
@@ -13,7 +13,7 @@ import inspect
 class mlogs(loader.Module):
     '''Модуль для логгирования в боте MineEvo'''
     strings = {
-        "name" : "mlogs", 
+        "name" : "mlogs_main", 
     }
     
     async def client_ready(self):
