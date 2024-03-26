b6d2d443df6489628831c7f124abb37c3b3fd62c
qqJIeee <mhabar08@mail.ru>
2024-03-26 07:10:30 +0300

    Update mcalc.py

--- mcalc.py
+++ mcalc.py
@@ -50,7 +50,7 @@ class mcalc(loader.Module):
     '''dev - @Kepperok'''
 
     strings = {"name" : "mcalc"}
-    locale.setlocale(locale.LC_ALL, 'us')
+    locale.setlocale(locale.LC_ALL, 'en-US')
 
     def formatd(self, value: int):
         return locale.format("%d", value, grouping=True)
